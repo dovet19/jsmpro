21bdcdc793f5211cfd4525702e8a977cd9caabb6
dovet19 <haebinlim@gmail.com>
2024-03-13 20:36:14 -0600

    Sanity, filtering, search

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,25 @@
 import SearchForm from "@/components/SearchForm";
 import Filters from "@/components/Filters";
+import { getResources, getResourcesPlaylist } from "@/sanity/actions";
+import ResourceCard from "@/components/ResourceCard";
+import Header from "@/components/Header";
 
-const Page = () => {
+// refresh cache every 900 sec
+export const revalidate = 900;
+
+interface Props {
+  searchParams: { [key: string]: string | undefined };
+}
+
+const Page = async ({ searchParams }: Props) => {
+  const resources = await getResources({
+    query: searchParams?.query || "",
+    category: searchParams?.category || "",
+    page: "1",
+  });
+
+  const resourcesPlaylist = await getResourcesPlaylist();
+
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
@@ -13,6 +31,31 @@ const Page = () => {
         <SearchForm />
       </section>
       <Filters />
+
+      {(searchParams?.query || searchParams?.category) && (
+        <section className="flex-center mt-6 w-full flex-col sm:mt-20">
+          <Header
+            title="Resources"
+            query={searchParams?.query || ""}
+            category={searchParams?.category || ""}
+          />
+          <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+            {resources?.length > 0 ? (
+              resources.map((resource: any) => (
+                <ResourceCard
+                  key={resource._id}
+                  title={resource.title}
+                  id={resource._id}
+                  image={resource.image}
+                  downloadNumber={resource.views}
+                />
+              ))
+            ) : (
+              <p className="body-regular text-white-400">No resouces found</p>
+            )}
+          </div>
+        </section>
+      )}
     </main>
   );
 };
--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,11 +1,41 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import Image from "next/image";
-import { useState } from "react";
 import { Input } from "./ui/input";
+import { formUrlQuery } from "@/sanity/utils";
 
 const SearchForm = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    // wait 300ms before searching
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
+
+      if (search) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: search,
+        });
+      } else {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          keysToRemove: ["query"],
+        });
+      }
+      router.push(newUrl, { scroll: false });
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search]);
+
   return (
     <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
       <label className="flex-center relative w-full max-w-3xl">
